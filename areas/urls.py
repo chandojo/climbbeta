d2771a7eea51410be03687bccbbf2a1c5e45c4d8
Celena Handojo <cmhandojo@gmail.com>
2018-12-04 09:52:16 -0800

    added weather api to areas

--- areas/urls.py
+++ areas/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'areas'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug:slug>/', views.state_view, name='states'),
+#    path('<slug:slug>/', views.state_view, name='states'),
+    path('<slug:slug>/', views.State_View.as_view(), name='states'),
+#    path('<slug:state__slug>/<slug:slug>/', views.City_View, name='cities')
     path('<slug:state__slug>/<slug:slug>/', views.city_view, name='cities')
 ]
--- areas/views.py
+++ areas/views.py
@@ -1,9 +1,12 @@
+import requests
+
 from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
 from django.template import loader
 from areas.models import *
+from api.weather.api import url
 
 # Create your views here.
 def index(request):
@@ -15,25 +18,52 @@ def index(request):
 
     return render(request, template, context)
 
-def state_view(request, slug):
-    state = get_object_or_404(State, slug=slug)
+#def state_view(request,slug):
+#    template = 'areas/state.html'
+#    state = get_object_or_404(State, slug=slug)
+#    city_list = City_Town.objects.all()
+#    context = {'state':state,'city_list':city_list}
+#    return render(request, template, context)
 
-    area_list = Area.objects.all()
-    city_list = City_Town.objects.all()
-    boulder_list = Boulder_Wall.objects.all()
 
+
+class State_View(ListView):
     template = 'areas/state.html'
 
-    context = {'state':state, 'area_list':area_list, 'city_list':city_list, 'boulder_list':boulder_list}
+    def get_queryset(self):
+        self.state = get_object_or_404(State, slug=self.kwargs['slug'])
+        return City_Town.objects.filter(state__name=self.state)
 
+    def get_weather_data(self, **kwargs):
+        cities = City_Town.objects.filter(state__name=self.state)
+        self.weather_data = []
+
+        for city in cities:
+            r = requests.get(url.format(city)).json()
+
+            city_weather = {
+                "city": city.name,
+                "temperature": r['main']['temp'],
+                "description": r['weather'][0]['description'],
+                "icon": r['weather'][0]['icon'],
+            }
+
+            self.weather_data.append(city_weather)
+
+        return self.weather_data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['state'] = self.state
+        context['cities'] = self.get_queryset
+        context['cityWeather'] = self.get_weather_data
+        return context
+
+
+
+def city_view(request, slug, state__slug):
+    city = get_object_or_404(City_Town, slug=slug, state__slug=state__slug)
+    area_list = Area.objects.all()
+    template = 'areas/city.html'
+    context = {'city':city, 'area_list':area_list}
     return render(request, template, context)
-
-#class City_View(View):
-#    template = 'areas/city.html'
-#
-#    def city_view(request, slug, state__slug):
-#        city = get_object_or_404(City_Town, slug=slug, state__slug=state__slug)
-#        area_list = Area.objects.all()
-#        template = 'areas/city.html'
-#        context = {'city':city, 'area_list':area_list}
-#        return render(request, template, context)
