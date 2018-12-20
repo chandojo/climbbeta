2c1975d688f6f4c6d724c8bf91d244debc1d0514
Celena Handojo <cmhandojo@gmail.com>
2018-12-20 15:08:42 -0800

    added jquery ui

--- climbbeta/urls.py
+++ climbbeta/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-#if settings.DEBUG:
+if settings.DEBUG:
 #    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
