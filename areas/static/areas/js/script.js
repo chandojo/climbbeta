3fcbca5267973283379a35d00fb91beac56ba059
Celena Handojo <cmhandojo@gmail.com>
2018-12-20 14:20:23 -0800

    added styling and jquery ui to city view page

---
+++ areas/static/areas/js/script.js
@@ -0,0 +1,9 @@
+$('.video-card').on('click', function(){
+  $('.video-card.active-video').removeClass('active-video');
+
+  var embed = $('iframe', this).attr('src');
+  var htmlEmbed = embed + "&autoplay=1"
+  $('.video').attr('src', htmlEmbed);
+  
+  $(this).addClass('active-video');
+});
--- climbbeta/urls.py
+++ climbbeta/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('weather/', include('weather.urls')),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
