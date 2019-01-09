cafb591cb6e26a210d3c8a07bbf6486389eaf2eb
Celena Handojo <cmhandojo@gmail.com>
2019-01-09 12:46:50 -0800

    uncommented static settings

--- climbbeta/settings/production.py
+++ climbbeta/settings/production.py
@@ -126,10 +126,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
