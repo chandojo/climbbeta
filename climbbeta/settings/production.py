0f8b462c71e8989a3dcfb0eb08fb4d3b842cea49
Celena Handojo <cmhandojo@gmail.com>
2019-01-08 16:33:26 -0800

    added 'admin' section for logging

--- climbbeta/settings/production.py
+++ climbbeta/settings/production.py
@@ -151,22 +151,6 @@ SECURE_BROWSER_XSS_FILTER       = True
 X_FRAME_OPTIONS                 = 'DENY'
 SECURE_HSTS_PRELOAD             = True
 
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+ADMINS = (('Celena', 'cmhandojo@gmail.com'),)
 
 django_heroku.settings(locals())
