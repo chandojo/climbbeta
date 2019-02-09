7b5a35d3e851cb636a85f685c3e370ab70357bbf
Celena Handojo <cmhandojo@gmail.com>
2019-02-09 10:28:19 -0800

    react-router-dom installed. navbar (non-functional) installed

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -3,16 +3,15 @@ import React, { Component } from 'react';
 export class Header extends Component {
   render(){
     return (
-      <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">Climb Beta</a>
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-
-          </ul>
-
+        <div class="collapse navbar-collapse" id="navbarText">
+            <div class="navbar-nav mx-auto">
+              <a class="nav-link nav-item" href="#">Areas</a>
+              <a class="nav-link nav-item" href="#">Members</a>
+            </div>
         </div>
       </nav>
     );
