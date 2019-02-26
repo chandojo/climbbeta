dc96b20a5e3bd189f28cfd55ee48a7a208d6e4f5
Celena Handojo <cmhandojo@gmail.com>
2019-02-26 12:52:12 -0800

    error handling climbing vid

--- frontend/src/components/videos/ClimbingVideos.js
+++ frontend/src/components/videos/ClimbingVideos.js
@@ -5,13 +5,14 @@ class ClimbingVideos extends Component {
     constructor(props){
       super(props)
       this.state = {
+        error: null,
         isLoaded: false,
         climbingVideos: []
       };
     }
 
     componentDidMount(){
-      let endpoint = '/videos/api/videos/'
+      let endpoint = '/videos/api/videos'
       let thisComp = this
       let lookupOptions = {
         method: "GET",
@@ -20,32 +21,38 @@ class ClimbingVideos extends Component {
         }
       }
 
-      fetch(endpoint, lookupOptions)
+      fetch(endpoint,lookupOptions)
       .then(function(response){
         return response.json()
       }).then(function(responseData){
-          console.log(responseData);
-          thisComp.setState({
-            isLoaded: true,
-            climbingVideos:responseData
-          })
-      });
+        thisComp.setState({
+          isLoaded:true,
+          climbingVideos: responseData
+        })
+      }).catch(function(error){
+        thisComp.setState({
+          error,
+          isLoaded:true,
+        })
+          console.log("Error: ", error);
+      })
     }
 
     render(){
+          const { error } = this.state;
           const { isLoaded } = this.state;
           const { climbingVideos } = this.state;
           const totalVideos = climbingVideos.length;
           return(
             <div>
-              { !isLoaded ? (
-                <div>Loading...</div>
-              ) : (
-                <div>
-                  <p>It works! </p>
-                  { climbingVideos[0].name }
-                 </div>
-              )}
+            {
+              !isLoaded ?
+                (<div>Loading...</div>)
+               : isLoaded && totalVideos > 0 ?
+                (<div> it works! </div>)
+              : isLoaded && error !== null ?
+                (<div> an error has occured </div>) : ""
+            }
             </div>
           )
         }
