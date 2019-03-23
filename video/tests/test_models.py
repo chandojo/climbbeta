7a361b6ca0fcb278011c85e797fcb2f3fdbf91b1
Celena Handojo <cmhandojo@gmail.com>
2019-03-23 12:40:50 -0700

    [PROBLEM]: 	Data needs to be tested for Videos model. [SOLUTION]: 	Added fake value and tested expected result.

--- video/tests/test_models.py
+++ video/tests/test_models.py
@@ -4,7 +4,7 @@ from areas.models import *
 from video.models import *
 
 
-class VideosTestCase(TestCase):
+class VideosLabelTestCase(TestCase):
     def test_videos_name_label(self):
         field_label = Videos._meta.get_field('name').verbose_name
         self.assertEquals(field_label, 'name')
@@ -45,10 +45,35 @@ class VideosTestCase(TestCase):
         max_length = Videos._meta.get_field('thumbnail').max_length
         self.assertEquals(max_length, 200)
 
+
+class VideosContentTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.state = State.objects.create(name='Washington')
+        cls.city = City_Town.objects.create(
+            name='Leavenworth', state=cls.state)
+        Videos.objects.create(name='This is a test', city=cls.city, author="Freddie Mercury",
+                              thumbnail="https://www.example.com/example.jpg", embed="sdf34bg", description="this is a description for a test")
+        cls.video = Videos.objects.get(name='This is a test')
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+        #cls.video = None
+
+    def test_city_is_name(self):
+        self.assertEquals(str(self.video.city), 'Leavenworth')
+
+    def test_url_is_http(self):
+        self.assertIn('https://', self.video.thumbnail)
+
+    def test_url_is_name(self):
+        self.assertEquals(self.video.thumbnail,
+                          'https://www.example.com/example.jpg')
+
+    def test_author_is_name(self):
+        self.assertEquals(self.video.author, 'Freddie Mercury')
+
     def test_slug_is_name(self):
-        state = State.objects.create(name='Washington')
-        city = City_Town.objects.create(name='Leavenworth', state=state)
-        Videos.objects.create(name='This is a test', city=city)
-        slug_title = 'this-is-a-test'
-        videos = Videos.objects.get(name='This is a test')
-        self.assertEquals(videos.slug, slug_title)
+        self.assertEquals(self.video.slug, 'this-is-a-test')
