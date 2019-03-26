8411e1d3990e182b32900da0de03fb1954122b85
Celena Handojo <cmhandojo@gmail.com>
2019-03-26 08:14:11 -0700

    [PROBLEM]: Testing for external api call needed. [SOLUTION]: Added 'if status_code == 200' prior to collecting json data from response. [NOTES]: More thorough external api tests can be added.

--- video/api/get_all_videos.py
+++ video/api/get_all_videos.py
@@ -11,45 +11,54 @@ from areas.models import *
 def get_all_city_videos():
     cities = City_Town.objects.all()
     today = str(date.today())
+
     for city in cities:
         get_city = str(city.name)
         no_space_city = get_city.replace(' ', '')
         q = get_city + ' bouldering | ' + get_city + \
             ' ' + str(city.state.name) + ' bouldering'
         vimeo_response = client.get(video_url.format(
-            q), params={"fields": "uri, name, description, embed, pictures, user"}).json()
+            q), params={"fields": "uri, name, description, embed, pictures, user"}, headers={"content-type": "application/json"}).json()
         youtube_response = requests.get(
-            keys.youtube_search_url.format(q)).json()
-        video_response = 'video/fixtures/' + today + \
-            no_space_city + '-' + str(city.state.abbrv) + '.json'
+            keys.youtube_search_url.format(q), headers={"content-type": "application/json"}).json()
+        video_response = 'video/fixtures/' + today +
+        no_space_city + '-' + str(city.state.abbrv) + '.json'
         video_results = []
 
-        for video in vimeo_response['data']:
-            vimeo_video = {
-                'model': 'video.Videos',
-                'fields': {
-                    'name': video['name'],
-                    'city': get_city,
-                    'author': video['user']['name'],
-                    'thumbnail': video['pictures']['sizes'][2]['link'],
-                    'embed': video['embed']['html'],
-                    'description': video['description'],
+        if vimeo_response.status_code == 200:
+            for video in vimeo_response['data']:
+                vimeo_video = {
+                    'model': 'video.Videos',
+                    'fields': {
+                        'name': video['name'],
+                        'city': get_city,
+                        'author': video['user']['name'],
+                        'thumbnail': video['pictures']['sizes'][2]['link'],
+                        'embed': video['embed']['html'],
+                        'description': video['description'],
+                    }
                 }
-            }
-            video_results.append(vimeo_video)
-        for video in youtube_response['items']:
-            youtube_video = {
-                'model': 'video.Videos',
-                'fields': {
-                    'name': video['snippet']['title'],
-                    'city': get_city,
-                    'author': video['snippet']['channelTitle'],
-                    'thumbnail': video['snippet']['thumbnails']['medium']['url'],
-                    'embed': video['id']['videoId'],
-                    'description': video['snippet']['description'],
+                video_results.append(vimeo_video)
+        else:
+            print("There has been an error with the Vimeo call")
+
+        if youtube_response.status_code == 200:
+            for video in youtube_response['items']:
+                youtube_video = {
+                    'model': 'video.Videos',
+                    'fields': {
+                        'name': video['snippet']['title'],
+                        'city': get_city,
+                        'author': video['snippet']['channelTitle'],
+                        'thumbnail': video['snippet']['thumbnails']['medium']['url'],
+                        'embed': video['id']['videoId'],
+                        'description': video['snippet']['description'],
+                    }
                 }
-            }
-            video_results.append(youtube_video)
+                video_results.append(youtube_video)
+        else:
+            print("There has been an error with the YouTube call")
+
         with open(video_response, 'w') as f:
             json.dump(video_results, f)
 
@@ -63,8 +72,8 @@ def compile_videos():
     for city in cities:
         get_city = str(city.name)
         no_space_city = get_city.replace(' ', '')
-        video_response = 'video/fixtures/' + today + \
-            no_space_city + '-' + str(city.state.abbrv) + '.json'
+        video_response = 'video/fixtures/' + today +
+        no_space_city + '-' + str(city.state.abbrv) + '.json'
         with open(video_response) as f:
             video_file = json.load(f)
             for video in video_file:
--- video/api/keys.py
+++ video/api/keys.py
@@ -2,10 +2,10 @@ import vimeo
 import os
 
 client = vimeo.VimeoClient(
-       token=os.environ['VIMEO_ACCESS_TOKEN'],
-       key=os.environ['VIMEO_CLIENT_ID'],
-       secret=os.environ['VIMEO_CLIENT_SECRET']
-    )
+    token=os.environ['VIMEO_ACCESS_TOKEN'],
+    key=os.environ['VIMEO_CLIENT_ID'],
+    secret=os.environ['VIMEO_CLIENT_SECRET']
+)
 
 video_url = '/videos?query={}&direction=desc&sort=relevant&per_page=20'
 
