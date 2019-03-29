3dce4b83d2b329d5833bd491a349f8387dc22150
Celena Handojo <cmhandojo@gmail.com>
2019-03-29 09:58:53 -0700

    [PROBLEM]: Unused apps cluttering project
    [SOLUTION]: Remove members/ app from project
    
    [NOTES]:

--- members/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Member(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=50)
