c974c68ebcfc78ff8d9ec1ec461d51972633446c
Celena Handojo <cmhandojo@gmail.com>
2019-04-17 08:33:28 -0700

    [ISSUE]: #12 Weather API
    [PROBLEM]: Need to fetch weather for city
    
    [SOLUTION]: Call weather API through cityDetails function in CityDetail component
    
    [NOTES]: Potentially find way to cache weather data to avoid constant re-fetching

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,7 +4,22 @@ import { BrowserRouter, Link } from 'react-router-dom';
 class Header extends Component {
   render(){
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+  /*    <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+*/
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mt-3">
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
