41abc8132ad29d2ae4955259d8d3355f633a56a9
Celena Handojo <cmhandojo@gmail.com>
2019-04-24 20:09:50 -0700

    [PROBLEM]: Need to render weather forecast per city
    [SOLUTION]: Weather forecast API response rendered in tabbed section called "forecast"

--- frontend/src/components/layout/WeatherForecast.js
+++ frontend/src/components/layout/WeatherForecast.js
@@ -6,27 +6,28 @@ export default class WeatherForecast extends Component {
       const { weatherForecast } = this.props;
       console.log(weatherForecast)
       return(
-        <>
+        <div className="row">
         { Object.keys(weatherForecast).map((day, key)=>{
           return(
-            <div key={key}>
-              { weatherForecast[day].map((value, key)=>{
-                return(
-                  <div key={key}>
-                  { Object.keys(value).map((item, key)=>{
-                    return(
-                      <p key={key}>{ value[item] }</p>
-                    )
-                  })}
-                </div>
+              <div className="col text-center border border-light">
+                  <h5 className="mt-1 pt-3 pb-3 bg-warning">{ day }</h5>
 
-                )
-              })}
+                { weatherForecast[day].map((value, key)=>{
+                  var icon = `http://openweathermap.org/img/w/${ value.icon }.png`;
+                      return(
+                        <div className="card mx-auto text-center border-0">
+                          <div className="card-header">{ value.dt.slice(10) }</div>
+                          <img className=" mx-auto d-block" src={ icon }/>
+                          <div className="card-text">
+                            <p className="text-capitalize">{ value.temp}&#176;F { value.desc }</p>
+                          </div>
+                        </div>
+                      )
+                    })}
             </div>
-
             )
         })}
-      </>
+      </div>
       )
     }
   }
