6937d03d86866c19eae1acf797d7ff95d8fe008f
Celena Handojo <cmhandojo@gmail.com>
2019-05-03 13:07:06 -0700

    [PROBLEM]: Mixed content error in production
    [SOLUTION]: http to https in openweathermap icon

--- frontend/src/components/layout/WeatherForecast.js
+++ frontend/src/components/layout/WeatherForecast.js
@@ -12,7 +12,7 @@ export default class WeatherForecast extends Component {
                   <h5 className="mt-1 pt-3 pb-3 bg-warning">{ day }</h5>
 
                 { weatherForecast[day].map((value, key)=>{
-                  var icon = `http://openweathermap.org/img/w/${ value.icon }.png`;
+                  var icon = `https://openweathermap.org/img/w/${ value.icon }.png`;
                       return(
                         <div className="card mx-auto text-center border-0" key={key}>
                           <div className="card-header">{ value.dt.slice(10) }</div>
