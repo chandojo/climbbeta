eb32d0330df3ddf038103644554b08d019c28de1
Celena Handojo <cmhandojo@gmail.com>
2019-05-13 13:58:05 -0700

    [ISSUE]: #15 Add footer with link to github
    [PROBLEM]: Footer needed with about and contribution pages.
    
    [SOLUTION]: Add footer with links and info

---
+++ frontend/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () =>(
+  <div>
+    <p>This is the About page</p>
+  </div>
+);
+
+export default About;
---
+++ frontend/src/components/Contribute.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contribute = () =>(
+  <div>
+    <span>Github</span>
+  </div>
+);
+
+export default Contribute;
