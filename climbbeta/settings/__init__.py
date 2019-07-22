e21de4d05a8d67ada6276ecb4d576f45c3cba79d
Celena Handojo <cmhandojo@gmail.com>
2019-07-22 10:22:19 -0700

    [ISSUE]: Local/Production settings config #39
    [PROBLEM]: Hassle staging commits because must remove init file to ensure production settings are used.
    
    [SOLUTION]: Added IF statement and production_setting environment variable to ease staging and commiting changes
    
    [NOTES]:

--- climbbeta/settings/__init__.py
+++ climbbeta/settings/__init__.py
@@ -1,2 +1,9 @@
-#from .local import *
-from .production import *
+import os
+import sys
+
+if os.environ['PRODUCTION_SETTING'] == "true":
+    from .production import *
+elif os.environ['PRODUCTION_SETTING'] == "false":
+    from .local import *
+else:
+    sys.exit("PRODUCTION_SETTING value is not recognized. Set variable to either 'true' or 'false'")
