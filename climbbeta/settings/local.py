fe7a12d45628861e2df4a0b847d1323dad221683
Celena H <cmhandojo@gmail.com>
2020-08-16 09:26:31 -0700

    Dockerfile, docker-compose files added for local development

--- climbbeta/settings/local.py
+++ climbbeta/settings/local.py
@@ -15,21 +15,14 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'postgres',
-#        'USER': 'postgres',
-#        'PASSWORD': 'postgres',
-#        'HOST': 'db',
-#        'PORT': 5432,
-#    }
-#}
 
 
 # Internationalization
