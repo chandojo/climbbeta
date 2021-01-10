2da673d0de902509a9476832ee9ebd2e9f2ef5a6
chandojo <celena.handojo+github@gmail.com>
2021-01-10 12:42:40 -0800

    Release/1.5.0 (#63)
    * Optimized settings to bring common settings into common.py and use local.py as default settings
    
    * README updated
    
    * Dockerfile, docker-compose files added for local development
    
    * Modified Docker and production.py file
    
    * changed database settings back
    
    * added config to dockerfile
    
    * updated dockerfile, wsgi, and changelog
    
    * remove pip from requirements
    
    * abstract dockerfile variables to get env variables
    
    * updated readme
    
    * added to gitignore
    
    * Optimized settings to bring common settings into common.py and use local.py as default settings
    
    * README updated
    
    * Dockerfile, docker-compose files added for local development
    
    * Modified Docker and production.py file
    
    * changed database settings back
    
    * added config to dockerfile
    
    * updated dockerfile, wsgi, and changelog
    
    * remove pip from requirements
    
    * abstract dockerfile variables to get env variables
    
    * updated readme
    
    * added to gitignore
    
    * Pipeline/travisci integration (#62)
    
    * .travis.yml added
    
    * script and more before_install commands added
    
    * variables added to web environment docker-compose
    
    * travisci jobs and stages added
    
    * mounted app volume
    
    * comment out tests to be fixed in different branch
    
    * .travis.yml added
    
    * script and more before_install commands added
    
    * variables added to web environment docker-compose
    
    * travisci jobs and stages added
    
    * mounted app volume
    
    * comment out tests to be fixed in different branch
    
    * removed s3transfer version
    
    * removed tupsy, pycurl version
    
    Co-authored-by: Celena H <cmhandojo@gmail.com>
    
    * Updated changelog and readme
    
    * Heroku docker build (#64)
    
    * added heroku.yml
    * added heroku deployment to travisci
    
    * updated configuration to travisci

--- climbbeta/settings/production.py
+++ climbbeta/settings/production.py
@@ -1,82 +1,18 @@
+from climbbeta.settings.common import *
+
 import dj_database_url
-import os
 from climbbeta.aws.conf import *
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# Note: Production BASE_DIR different from local settings
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('CLIMB_BETA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['climbbeta.herokuapp.com', '.herokuapp.com', 'www.vplusbeta.com', 'vplusbeta.com']
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    'areas',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'vimeo',
-    'timezonefinder',
-    'requests_cache',
-    'geopy',
-    'storages',
-    'rest_framework',
-    'django_filters',
-    'video',
-    'frontend'
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
-
-ROOT_URLCONF = 'climbbeta.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'climbbeta.wsgi.application'
+ALLOWED_HOSTS = ['localhost','climbbeta.herokuapp.com', '.herokuapp.com']
 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
@@ -89,60 +25,6 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-LOGIN_REDIRECT_URL = '/weather'
-LOGIN_URL = '/members/login'
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticatedOrReadOnly',)
-}
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-#MEDIA_ROOT = BASE_DIR
-#MEDIA_URL = '/media/'
 
 CORS_REPLACE_HTTPS_REFERER = True
 HOST_SCHEME = "https://"
